fd33671037148ea878f114a0e77c63cc8d1b64d5
Anzel Lai <anzel_lai@hotmail.com>
2019-05-10 15:00:17 +0100

    fix: fetch all available comments

--- pkg/scratch/apis.go
+++ pkg/scratch/apis.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	scratchProjectAPI = "https://api.scratch.mit.edu/explore/projects?limit=%d&offset=%d&language=%s&mode=%s"
-	scratchCommentAPI = "https://api.scratch.mit.edu/projects/%d/comments?offset=%d"
+	scratchCommentAPI = "https://api.scratch.mit.edu/projects/%d/comments?limit=40&offset=%d"
 )
 
 // API struct ...
@@ -46,11 +46,22 @@ func (api *API) GetProjects() (projects []Project, err error) {
 
 // GetComments return slice of Comments for its ProjectID from Scratch API
 func (api *API) GetComments(projectID int) (comments []Comment, err error) {
-	r, err := req.Get(fmt.Sprintf(scratchCommentAPI, projectID, api.Offset))
-	if err != nil {
-		return
+	offset := 0
+	for {
+		cmts := []Comment{}
+		commentsEndpoint := fmt.Sprintf(scratchCommentAPI, projectID, offset)
+		fmt.Printf("calling Scratch comments endpoint: %s\n", commentsEndpoint)
+		r, err := req.Get(commentsEndpoint)
+		if err != nil {
+			return comments, err
+		}
+		r.ToJSON(&cmts)
+		if len(cmts) == 0 {
+			return comments, nil
+		}
+		comments = append(comments, cmts...)
+		offset += 40
 	}
-	r.ToJSON(&comments)
 	return
 }
 
@@ -85,6 +96,7 @@ func (api *API) GetAnalysis() error {
 	lang := "en"
 	for _, result := range api.Results {
 		for _, review := range result.Reviews {
+			fmt.Printf("calling AWS Comprehend for: %v\n", review)
 			summary, err := srv.DetectSentiment(&comprehend.DetectSentimentInput{LanguageCode: &lang, Text: &review.Content})
 			if err != nil {
 				fmt.Printf("error using comprehend: %v", err)
